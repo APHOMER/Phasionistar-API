603f10e2a74b35b90ae7a47002be758e7b3b68b7
Aphomer <aaphomer@gmail.com>
2023-07-25 13:02:50 +0100

    adding CORS to App.js

--- app.js
+++ app.js
@@ -24,13 +24,14 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.urlencoded({ extended: false })); // x-ww-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
 app.use(cors());
+
 // CORS SETTINGS
-// app.use((req, res, next) => {
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-//     next();
-// })
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+})
 
 app.use('/', homeRouter);
 app.use('/user', userRouter);
